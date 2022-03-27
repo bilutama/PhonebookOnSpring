252231f7ce5bc171dc6cb51c7f0b5f5f23943033
Maksim Biluta <biluta.m.a@gmail.com>
2022-03-27 23:26:11 +0200

    PhonebookHibernate

--- src/main/resources/static/js/libs/jquery.js
+++ src/main/resources/static/js/libs/jquery.js
@@ -1381,6 +1381,7 @@ setDocument = Sizzle.setDocument = function( node ) {
 
 			// Support: Firefox <=3.6 - 5 only
 			// Old Firefox doesn't throw on a badly-escaped identifier.
+			// noinspection SyntaxError
 			el.querySelectorAll( "\\\f" );
 			rbuggyQSA.push( "[\\r\\n\\f]" );
 		} );
--- src/main/resources/static/js/phonebook.js
+++ src/main/resources/static/js/phonebook.js
@@ -169,25 +169,6 @@ new Vue({
             });
         },
 
-        exportContacts() {
-            //window.open(("/phonebook/export"), "_blank");
-            // TODO: implement excel export
-            $.ajax({
-                type: "GET",
-                url: "/phonebook/rpc/api/v1/export",
-            }).done(response => {
-                const url = window.URL.createObjectURL(new Blob([response]));
-                const link = document.createElement("a");
-                link.href = url;
-                link.setAttribute("download", "phonebook.xlsx");
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }).fail(ajaxRequest => {
-                console.log(ajaxRequest);
-            });
-        },
-
         loadData(term) {
             let termPath = "";
 
