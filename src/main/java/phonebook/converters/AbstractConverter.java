585e2a206f7ac8cee2589ed325becda93f4df8d3
Maxim Biluta <biluta.m.a@gmail.com>
2023-09-26 00:12:34 +0200

    formatting

--- src/main/java/phonebook/converters/AbstractConverter.java
+++ src/main/java/phonebook/converters/AbstractConverter.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class AbstractConverter<S, D> implements Converter<S, D> {
-    @Override
-    public List<D> convert(List<S> source) {
-        return source.stream().map(this::convert).collect(Collectors.toList());
-    }
+	@Override
+	public List<D> convert(List<S> source) {
+		return source.stream().map(this::convert).collect(Collectors.toList());
+	}
 }
