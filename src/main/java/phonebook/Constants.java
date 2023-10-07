2494c7e288585c5c1848674fc809cd915a752f3f
Maxim Biluta <biluta.m.a@gmail.com>
2023-10-08 00:51:38 +0200

    Validate messages moved to a separate source

--- src/main/java/phonebook/Constants.java
+++ src/main/java/phonebook/Constants.java
@@ -7,6 +7,9 @@ public final class Constants {
 
 	public static final class ContactService {
 
+		private ContactService() {
+		}
+
 		public static final String FIRST_NAME_VALIDATION_ERROR_MESSAGE = "First name is required";
 
 		public static final String LAST_NAME_VALIDATION_ERROR_MESSAGE = "Last name is required";
